b221826d9727bd089b29754b84deff6a846ab23b
kawamataryo <ba068082@gmail.com>
2022-05-03 18:08:47 +0900

    test: add coverage command

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     include: ["**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    coverage: {
+      reporter: ["text", "json", "html"],
+    },
   },
 });
