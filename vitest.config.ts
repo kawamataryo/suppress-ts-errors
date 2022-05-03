b0560ad0ff43f14c7b65b8beb9b07cf90515bb46
kawamataryo <ba068082@gmail.com>
2022-05-03 18:41:26 +0900

    Revert "test: add coverage command"
    This reverts commit b221826d9727bd089b29754b84deff6a846ab23b.

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,8 +3,5 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     include: ["**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    coverage: {
-      reporter: ["text", "json", "html"],
-    },
   },
 });
