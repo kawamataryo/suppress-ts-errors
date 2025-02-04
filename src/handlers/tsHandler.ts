66d9c3ead0ed0ea10fcd7c79f0f98a0425a788d6
ahan3 <albert_han@intuit.com>
2025-02-04 17:28:10 -0500

    fix: add error handling for tsHandler

--- src/handlers/tsHandler.ts
+++ src/handlers/tsHandler.ts
@@ -15,24 +15,38 @@ export const tsHandler = ({
   const progressBar = generateProgressBar();
   progressBar.start(sourceFiles.length, 0);
 
+  // Store files that threw an error
+  const failedFileArr = [];
+
   // Rewrite source in ts/tsx file with source with comment
   let insertedCommentCount = 0;
   for (const sourceFile of sourceFiles) {
-    const { text: textWithComment, count: insertedCommentCountPerFile } =
-      suppressTsErrors({
-        sourceFile,
-        commentType,
-        withErrorCode: errorCode,
-      });
+    try {
+      const { text: textWithComment, count: insertedCommentCountPerFile } =
+        suppressTsErrors({
+          sourceFile,
+          commentType,
+          withErrorCode: errorCode,
+        });
 
-    if (insertedCommentCountPerFile > 0) {
-      sourceFile.replaceWithText(textWithComment);
-      sourceFile.saveSync();
-      insertedCommentCount += insertedCommentCountPerFile;
+      if (insertedCommentCountPerFile > 0) {
+        sourceFile.replaceWithText(textWithComment);
+        sourceFile.saveSync();
+        insertedCommentCount += insertedCommentCountPerFile;
+      }
+      progressBar.increment();
+    } catch {
+      failedFileArr.push(sourceFile.getFilePath());
     }
-    progressBar.increment();
   }
 
   progressBar.stop();
+
+  if (failedFileArr.length > 0)
+    console.log(
+      "There were issues handling the following files:",
+      failedFileArr,
+      "Please review source files before re-running script",
+    );
   return insertedCommentCount;
 };
